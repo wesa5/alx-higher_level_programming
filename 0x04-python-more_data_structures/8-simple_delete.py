db3342d6741c32157aadb29d885bf1b0bfaaa50d
wesa5 <wobulufrancis@gmail.com>
2022-06-08 15:24:15 +0300

    delete

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-
-
 def simple_delete(a_dictionary, key=""):
-    """
-    A function that deletes a key in a dictionary.
-    """
-    if key in a_dictionary:
-        del a_dictionary[key]
+    a_dictionary.pop(key, None)
     return a_dictionary
