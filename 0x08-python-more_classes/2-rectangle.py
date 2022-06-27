263be82700930bc4945102cbdece92bc9dfebff0
wesa5 <wobulufrancis@gmail.com>
2022-06-27 12:44:03 +0300

    fix

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -8,20 +8,19 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
         """Property retriever, for retreiving"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
-        
-        if type(value) != int:
-            raise TypeError("Width must be an integer")
+        """Property setter, for setting"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        
         self.__width = value
 
     @property
@@ -33,27 +32,24 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        
         self.__height = value
-    
+
     def area(self):
         """Public instance method that returns
         the rectangle area
         """
-        self_area = self.__width * self.__height
-        return self_area
-    
+        rectangle_area = self.__height * self.__width
+        return rectangle_area
+
     def perimeter(self):
         """Public instance method that returns the
         rectangle perimeter
         """
         if self.__width == 0 or self.__height == 0:
-            return 0
-        self_perimeter = self.__width + self.__height + self.__width + self.__height
-
-        return self_perimeter
+            return (0)
+        rectangle_parameter = ((2 * self.__height) + (2 * self.__width))
+        return rectangle_parameter
